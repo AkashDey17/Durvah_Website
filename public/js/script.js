d8e1d015d804dcb83b34dddfd24931443a3e4dce
unknown <akashdeypersonal17@gmail.com>
2024-10-08 15:03:10 +0530

    Added Scrolling effect in production page both in services and products section

--- public/js/script.js
+++ public/js/script.js
@@ -1,32 +1,8 @@
-// const nav = document.querySelector(".nav"),
-//   searchIcon = document.querySelector("#searchIcon"),
-//   navOpenBtn = document.querySelector(".navOpenBtn"),
-//   navCloseBtn = document.querySelector(".navCloseBtn");
-
-// searchIcon.addEventListener("click", () => {
-//   nav.classList.toggle("openSearch");
-//   nav.classList.remove("openNav");
-//   if (nav.classList.contains("openSearch")) {
-//     return searchIcon.classList.replace("uil-search", "uil-times");
-//   }
-//   searchIcon.classList.replace("uil-times", "uil-search");
-// });
-
-// navOpenBtn.addEventListener("click", () => {
-//   nav.classList.add("openNav");
-//   nav.classList.remove("openSearch");
-//   searchIcon.classList.replace("uil-times", "uil-search");
-// });
-// navCloseBtn.addEventListener("click", () => {
-//   nav.classList.remove("openNav");
-// });
-
 //navbar
 
 // search-box open close js code
 let navbar = document.querySelector(".navbar");
 let searchBox = document.querySelector(".search-box .bx-search");
-// let searchBoxCancel = document.querySelector(".search-box .bx-x");
 
 searchBox.addEventListener("click", () => {
   navbar.classList.toggle("showInput");
@@ -37,7 +13,6 @@ searchBox.addEventListener("click", () => {
   }
 });
 
-// sidebar open close js code
 let navLinks = document.querySelector(".nav-links");
 let menuOpenBtn = document.querySelector(".navbar .bx-menu");
 let menuCloseBtn = document.querySelector(".nav-links .bx-x");
@@ -48,7 +23,6 @@ menuCloseBtn.onclick = function () {
   navLinks.style.left = "-100%";
 };
 
-// sidebar submenu open close js code
 let htmlcssArrow = document.querySelector(".htmlcss-arrow");
 htmlcssArrow.onclick = function () {
   navLinks.classList.toggle("show1");
@@ -61,3 +35,5 @@ let jsArrow = document.querySelector(".js-arrow");
 jsArrow.onclick = function () {
   navLinks.classList.toggle("show3");
 };
+
+//header 2
